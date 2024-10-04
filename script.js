68ef9fb2f8962babc7bdbb1d985c9c0385f122f4
Gabriel Richter Reynoso <garey91@gmail.com>
2024-10-04 22:15:46 +0200

    First

--- script.js
+++ script.js
@@ -21,12 +21,13 @@ class Player {
 }
 
 let category = "";
+const introDiv = document.querySelector('.intro');
 const categoryButtons = document.querySelectorAll('.category-buttons');
 const introContinueButton = document.getElementById('intro-continue-button');
 const namesDiv = document.querySelector('.names');
-const subtractPlayers = document.getElementById('subtract')
+const subtractPlayers = document.getElementById('subtract');
 const amountPlayersInput = document.getElementById('players-amount');
-const addPlayers = document.getElementById('add')
+const addPlayers = document.getElementById('add');
 const startGameButton = document.getElementById('start-game');
 let questions;
 const gameDiv = document.querySelector('.game');
@@ -34,7 +35,8 @@ const gameDiv = document.querySelector('.game');
 categoryButtons.forEach(button => {
     button.addEventListener('click', () => {
         category = button.value;
-        console.log(category);
+
+        button.style.backgroundColor = 'green';
     })
 })
 
@@ -54,12 +56,15 @@ addPlayers.addEventListener('click', () => {
 })
 
 introContinueButton.addEventListener('click', () => {
+    introDiv.classList.toggle('hidden');
+    namesDiv.classList.toggle('show');
     getQuestions(category);
     makeNameInputs();
 
 })
 
 startGameButton.addEventListener('click', () => {
+    namesDiv.classList.toggle('show');
     createPlayers();
     startGame();
 })
@@ -105,8 +110,15 @@ function makeNameInputs() {
     for (let i = 0; i < app.amountOfPlayers; i++) {
         const nameInput = document.createElement('input');
         nameInput.className = "name-input";
-        namesDiv.appendChild(nameInput);
+        namesDiv.prepend(nameInput);
     }
+    createHeader()
+}
+
+function createHeader() {
+    const h2 = document.createElement('h2');
+    h2.innerText = 'Enter player names';
+    namesDiv.prepend(h2);
 }
 
 function createPlayers() {
@@ -118,27 +130,60 @@ function createPlayers() {
 }
 
 function startGame() {
-        const contentDiv = document.createElement('div');
-        const question = document.createElement('h3');
-        const optionsDiv = document.createElement('div');
-        for(let i = 0; i < 4; i++) {
-            const optionButton = document.createElement('button');
-            optionButton.className = "option";
-            optionsDiv.appendChild(optionButton);
-        }
-        const nextQuestionBtn = document.createElement('button');
-        nextQuestionBtn.className = 'next-question';
+    createElementsAndloadNewQuestion();
 
-        
-        question.textContent = questions.results[0].question;
 
-        contentDiv.appendChild(question);
-        contentDiv.appendChild(optionsDiv);
-        contentDiv.appendChild(nextQuestionBtn);
-        console.log(contentDiv);
-        gameDiv.appendChild(contentDiv);
+}
 
-        nextQuestionBtn.addEventListener('click', loadNewQuestion);
+    let counter = 0;
+function createElementsAndloadNewQuestion() {
+    let answers = [
+        {
+            answer: questions.results[counter].correct_answer,
+            isCorrect: true
+        },
+        {
+            answer: questions.results[counter].incorrect_answers[0],
+            isCorrect: false
+        },
+        {
+            answer: questions.results[counter].incorrect_answers[1],
+            isCorrect: false
+        },
+        {
+            answer: questions.results[counter].incorrect_answers[2],
+            isCorrect: false
+        },
+    ]
+
+    gameDiv.innerHTML = "";
+    const category = document.createElement('h2');
+    category.textContent = questions.results[counter].category;
+    const question = document.createElement('h3');
+    const optionsDiv = document.createElement('div');
+    optionsDiv.className = 'options';
+    for (let i = 0; i < 4; i++) {
+        const optionButton = document.createElement('button');
+        optionButton.className = "option";
+        optionButton.value = answers[i].isCorrect;
+        optionButton.textContent = answers[i].answer;
+        optionsDiv.appendChild(optionButton);
+    }
+    const nextQuestionBtn = document.createElement('button');
+    nextQuestionBtn.className = 'next-question';
+
+
+    question.textContent = questions.results[counter].question;
+
+    gameDiv.appendChild(category);
+    gameDiv.appendChild(question);
+    gameDiv.appendChild(optionsDiv);
+    gameDiv.appendChild(nextQuestionBtn);
+    counter ++;
+    nextQuestionBtn.addEventListener('click', startGame);
+
+
+
 
 }
 
